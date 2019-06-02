df09afe9cf6fd9ddd47ff3d3a54ec5c702541d89
iampaavan <46580953+iampaavan@users.noreply.github.com>
2019-06-02 19:38:10 -0400

    Update code_01_03.py

--- code_01_03.py
+++ code_01_03.py
@@ -4,7 +4,7 @@ url = 'https://api.github.com/user'
 response = requests.get(url)
 print(response.json())
 
-response_auth = requests.get(url, headers={'Authorization': 'Bearer 44bc4d7853e7632e12bdbcdd3ffa6389c63a148e'})
+response_auth = requests.get(url, headers={'Authorization': 'Bearer alphanumeric'})
 
 git_response = response_auth.json()
 
